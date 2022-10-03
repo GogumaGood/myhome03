78f6d3aa575294f77c44d0728131ca2c7e81d9bf
Lee <leedaero777@gmail.com>
2022-10-03 22:22:35 +0900

    init

---
+++ src/main/java/com/godcoder03/myhome03/Myhome03Application.java
@@ -0,0 +1,13 @@
+package com.godcoder03.myhome03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myhome03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myhome03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/godcoder03/myhome03/Myhome03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder03.myhome03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myhome03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
